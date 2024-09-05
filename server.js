8f1284d0d7eb92add6eaf7fffbd17fb8ee3878a1
raju2002raju <151726988+raju2002raju@users.noreply.github.com>
2024-09-05 14:08:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/files', express.static(path.join(__dirname, 'uploads')));
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000', // Specify your frontend origin
+    origin: 'https://med-scribe-front-end.vercel.app/', // Specify your frontend origin
     credentials: true // Allow cookies and other credentials to be sent
 }));
 app.use(express.json());
